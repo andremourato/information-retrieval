7aeb5ce1ac460790fed5725f4d37170210e2c5ed
André Mourato <andremourato@ua.pt>
2020-10-27 01:29:01 +0000

    Added comments to the code and added a print to show the selected file

--- assignment1/assignment.py
+++ assignment1/assignment.py
@@ -44,29 +44,108 @@ def read_corpus(filename):
         return dic
 
 def get_filtered_tokens(string):
+    '''Removes non-alphabetic characters, sets string to lower case and
+    removes word with less than 3 characters
+    Parameters
+    ----------
+    string : string
+        The string from where the tokens will be extracted
+        
+    Returns
+    -------
+    tokens : list
+        The list of tokens that were extracted from the input string
+
+    get_filtered_tokens('within a larger purine run (A(n)G(n)) in this process, by a mechanism')
+    would return ['within', 'larger', 'purine', 'run', 'this', 'process', 'mechanism']
+    '''
     return [token for token in ''.join([ c if c.isalpha() else ' ' for c in string.lower()]).split()\
                 if len(token) >= 3]
 
 def filter_stop_words(tokens):
+    '''Removes the stop words from the list of input tokens
+    Parameters
+    ----------
+    tokens : list
+        The list of tokens
+        
+    Returns
+    -------
+    tokens_without_stopwords : list
+        The list of tokens without the stop words
+
+    filter_stop_words(['within', 'larger', 'purine', 'run', 'this', 'process', 'mechanism'])
+    would return ['within', 'larger', 'purine', 'run', 'process', 'mechanism']
+    '''
     return [token for token in tokens if token not in stopwords]
 
 def simple_tokenizer(document_dict):
-    # replaces all non-alphabetic characters by a space, 
-    # lowercases tokens,
-    # splits on whitespace, and
-    # ignores all tokens with less than 3 characters
+    '''A tokenizer that replaces all non-alphabetic characters by a space, lowercases
+    tokens, splits on whitespace, and ignores all tokens with less than 3 characters. 
+    Parameters
+    ----------
+    document_dict : dict
+        Dictionary that associates the doi of documents to their title + abstract.
+        
+    Returns
+    -------
+    document_dict : dict
+        Dictionary that associates the doi of documents to the list of tokens
+        contained in it
+    '''
     return {
         docID : get_filtered_tokens(string) \
             for (docID,string) in document_dict.items()
     }
 
 def improved_tokenizer(document_dict):
+    '''An improved tokenizer that replaces all non-alphabetic characters by a space, lowercases
+    tokens, splits on whitespace, and ignores all tokens with less than 3 characters. This tokenizer
+    also uses the Porter stemmer and applies a stopword filter
+    Parameters
+    ----------
+    document_dict : dict
+        Dictionary that associates the doi of documents to their title + abstract.
+        
+    Returns
+    -------
+    document_dict : dict
+        Dictionary that associates the doi of documents to the list of tokens
+        contained in it
+    '''
     return {
         docID: Stemmer.Stemmer('porter').stemWords(filter_stop_words(get_filtered_tokens(string))) \
             for (docID,string) in document_dict.items()
     }
 
 def indexer(document_dict):
+    '''Indexes all the input tokens, registering the total number of occurrences of a token
+    and the list of documents in which it appears
+    Parameters
+    ----------
+    document_dict : dict
+        Dictionary that associates the doi of documents to the list of tokens
+        
+    Returns
+    -------
+    count_index : dict
+        Dictionary that contains the token as the key and the number of occurences as the value.
+        Example: {
+            'incub': 4,
+            'period': 6,
+            'epidemiolog': 2,
+            'characterist': 2,
+            'novel': 5,
+            'coronaviru': 8,
+        }
+
+    document_index : dict
+        Dictionary that contains the token as the key and the list of documents as the value.
+        Example: {
+            'incub': ['10.3390/jcm9020538', '10.3390/jcm9020538', '10.3390/jcm9020538'],
+            'period': ['10.3390/jcm9020538']
+        }
+    '''
     count_index = {}
     document_index = {}
     for docID in document_dict:
@@ -87,6 +166,7 @@ if __name__ == '__main__':
         filename = 'all_sources_metadata_2020-03-13.csv'
     else:
         filename = sys.argv[1]
+    print('Reading from file',filename)
 
     #0 - Reads the file
     documents = read_corpus(filename)
